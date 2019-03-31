d980608b2bacbd49f271242e0ef54506493b1e92
Ngoc <thanhtyluven@gmail.com>
2019-03-31 17:17:15 +0700

    toast

--- app/src/main/java/com/framgia/MainActivity.java
+++ app/src/main/java/com/framgia/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "ssssss", Toast.LENGTH_SHORT).show();
     }
 
     @Override
