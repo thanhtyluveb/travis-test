c1b86b0edb0b31b1f9843a96922f96d37fa3a8f6
Ngoc <thanhtyluven@gmail.com>
2019-03-31 17:37:05 +0700

    toast thanh 1

--- app/src/main/java/com/framgia/MainActivity.java
+++ app/src/main/java/com/framgia/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "ssssss", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "master", Toast.LENGTH_SHORT).show();
     }
 
     @Override
